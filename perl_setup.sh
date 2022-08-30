3fe6197237fd35c8b0bc5336229b329fc0ef22ed
JA <Jakea350@protonmail.com>
2022-08-30 15:20:36 -0500

    Create perl_setup.sh

---
+++ perl_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# cpm is fast!
+curl -fsSL https://raw.githubusercontent.com/skaji/cpm/main/cpm | perl - install -g App::cpm
+
+# some good modules to have in your environment:
+cpm install DBI
+cpm install JSON
+cpm install YAML
+cpm install Moo
+cpm install DateTime
+cpm install XML::Simple
+cpm install Text::CSV
+cpm install WWW::Mechanize
+cpm install HTTP::Response
+cpm install Devel::Trace
