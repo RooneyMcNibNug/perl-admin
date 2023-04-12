207db94f9a774f85009de9b42a439f5c96307967
JA <Jakea350@protonmail.com>
2023-04-12 16:02:41 -0500

    Update rando_lbxd_watchlist.pl

--- non-work/rando_lbxd_watchlist.pl
+++ non-work/rando_lbxd_watchlist.pl
@@ -1,7 +1,10 @@
-## Cheapo's alternative to using the Letterboxd API, which is in beta and requires a signup
+## Cheapo's alternative to using the Letterboxd API, which is in beta and requires a signup.
 #
-## This requires you to login to letterboxd and export your watchlist
+## This requires you to login to letterboxd and export your watchlist.
 ## Example: https://letterboxd.com/rooneymcnibnug/watchlist/export/
+#
+## This script will go through your exported watchlist file, pick a random title for you to watch, 
+## and then check against a directory on your machine to see if you have a video file matching it.
 #
 ## Creative because I want to work to make fighting against indecisiveness lazy
 ## ..and to learn better Perl.
@@ -38,3 +41,17 @@ my $full_link = $response->base;
 # Print the random film, its release year, and its letterboxd.com link
 print "\e[35mYou should just watch...\e[0m\n";
 print "\e[32m$title ($year)\e[0m \e[35m-\e[0m\ \e[32m$full_link\e[0m\n";
+
+# Check for files with the title name
+print "Checking for files with the title name...\n";
+my @title_words = split /\s+/, $title;
+my @ignore_words = ("the", "and", "&", "A");
+@title_words = grep { !($_ ~~ @ignore_words) } @title_words;
+my $title_regex = join('.*', @title_words);
+my @matching_files = grep /$title_regex/i, glob('~/media/*'); # CHANGE your directory for file lookups here!
+if (@matching_files) {
+    print "Found the following files matching the title name:\n";
+    print join("\n", @matching_files), "\n";
+} else {
+    print "No files found matching the title name.\n";
+}
