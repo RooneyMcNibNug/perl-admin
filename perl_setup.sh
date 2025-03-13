29f6ed701a4bca89db697a4baf334c8311f84e8f
JA <Jakea350@protonmail.com>
2025-03-13 00:55:29 +0000

    some scraping modules I use a lot

--- perl_setup.sh
+++ perl_setup.sh
@@ -8,7 +8,7 @@
 (echo y; echo o conf prerequisites_policy follow; echo o conf commit) | cpan
 
 # some good modules to have in your environment:
-cpan Data::Printer Perl::Critic DBI JSON YAML Moo DateTime XML::Simple PDF::Collage Text::CSV WWW::Mechanize HTTP::Response Devel::Trace JSON::MaybeXS Mojo::UserAgent use LWP::Simple HTML::TreeBuilder REST::Client Text::Markdown
+cpan Data::Printer Perl::Critic DBI URI JSON YAML Moo DateTime XML::Simple PDF::Collage Text::CSV WWW::Mechanize HTTP::Response Devel::Trace JSON::MaybeXS Mojo::UserAgent use LWP::Simple LWP::UserAgent HTML::TreeBuilder HTML::Parser Mojo::DOM REST::Client Text::Markdown
 
 #Informational:
 cpan -l
